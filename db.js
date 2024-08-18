dfa9a71765df1988504ade3085e3658ec6669947
GhubSagar <sagarpandey2303@gmail.com>
2024-08-18 11:35:51 +0530

    Deploy test 2

--- db.js
+++ db.js
@@ -2,10 +2,9 @@
 //this is connection to database
 
 const mongoose= require('mongoose');
-const mongoURI= 'mongodb://localhost:27017/inotebook'
-
-
-
+const mongoURI= 'mongodb+srv://sapytrash:Zp0Nsf8T94vkESbv@cluster0.itrvi.mongodb.net/iNotebook'
+//mongodb+srv://sapytrash:Zp0Nsf8T94vkESbv@cluster0.itrvi.mongodb.net/
+// mongodb://localhost:27017/inotebook
 const connectToMongo= async ()=>{
       try {await mongoose.connect(mongoURI);
     console.log("Connected to Mongo Successfully!");
